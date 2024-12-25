4974c479b79ec91d20c2e6de5621d1e53f283a72
cyrus28214 <cyrus28214@gmail.com>
2024-12-25 09:30:40 +0800

    update courseware

--- courseware/4_example_code_2024/Line.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-#include "Line.h"
-
-void Line::print()
-{
-    std::cout << "This is print in Line" << std::endl;
-}
--- courseware/4_example_code_2024/Line.h
+++
@@ -1,9 +0,0 @@
-#include "Point.h"
-
-class Line {
-private:
-    Point start, end;
-
-public:
-    void print();
-};
